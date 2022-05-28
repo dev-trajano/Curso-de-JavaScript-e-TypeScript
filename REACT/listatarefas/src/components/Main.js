58be8c3d1166d549623ff44005b255dcc59adb4b
joanderson <trajanojoanderson@gmail.com>
2022-05-28 00:39:36 -0300

    Updating 🚀

--- REACT/listatarefas/src/components/Main.js
+++ REACT/listatarefas/src/components/Main.js
@@ -1,27 +1,34 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import "./Main.css";
+import { FaPlus } from 'react-icons/fa';
+
+import './Main.css';
 
 export default class Main extends Component {
-	state = {
-		novaTarefa:"",
-	};
+  state = {
+    novaTarefa: '',
+  };
 
-	handleChange= (e) =>{
-		this.setState({
-			novaTarefa: e.target.value
-		});
-	};
-	render(){
-		// const { novaTarefa } = this.state;
-		return (
-			<div className="main">
-				<h1>Lista de trarefas</h1>
-				<form action="#">
-					<input onChange={this.handleChange} type="text" />
-					<button type="submit">Emviar</button>
-				</form>
-			</div>
-		);
-	}
+  handleChange = (e) => {
+    this.setState({
+      novaTarefa: e.target.value,
+    });
+  };
+
+  render() {
+    const { novaTarefa } = this.state;
+
+    return (
+      <div className="main">
+        <h1>Lista de trarefas</h1>
+
+        <form action="#" className="form">
+          <input onChange={this.handleChange} type="text" value={novaTarefa} />
+          <button type="submit">
+            <FaPlus />
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
